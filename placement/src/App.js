33bf896f46c283af2dce023af4119f60e43c2c0e
Career-Bridge <Career-Bridge>
2024-01-06 13:24:32 +0530

    Initial commit

---
+++ placement/src/App.js
@@ -0,0 +1,23 @@
+import Footer from './Footer.js';
+import Routing from './Routing.js';
+import Home from './component/Home.js';
+import Reasoning from './component/Reasoning.js';
+import SignUp from './component/SignUp.js';
+import Login from './component/Login.js';
+import Categories from './component/Categories.js';
+import NavBar from './component/NavBar.js';
+// import Reasoning from './component/Reasoning.js';
+
+
+function App() {
+  return (
+    <div>
+      {/* <SignUp></SignUp> */}
+      {/* <NavBar/> */}
+      <Routing/>
+      {/* <Footer/> */}
+    </div>
+  );
+}
+
+export default App;
